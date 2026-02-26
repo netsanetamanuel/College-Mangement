bc94ba4a3e3ade3fa0d1b206fb76b83fbf9a1c51
netsanetamanuel <abrehotamanuell@gmail.com>
2026-02-26 02:32:44 -0600

    chore : gitignore added

--- src/cms/Auth.java
+++ src/cms/Auth.java
@@ -13,22 +13,28 @@ public class Auth {
     }
     
     
-	public boolean adminLogin(String username,String password) {
+	public boolean adminLogin(String username,String password) { 
 		
-		// prepare statement to store username from db 
+	
+		
+		// prepare statement to store username and password  from db 
 		try {
 			
 			if(username.isEmpty()||password.isEmpty()) {
 				System.out.println("username or password don't exist!");
 				return false;
 			}
+			
+			// preparedstatement class to sotre username in statment object 
 			PreparedStatement statement = connection.prepareStatement("SELECT password FROM admin WHERE username = ?");
+			// statement object to store username from database 
 			statement.setString(1,username);
 			
-			// 
+			//fetch the result statement from after excutign query 
 			ResultSet resultset = statement.executeQuery();
 			
 			if(!resultset.next()) {
+				// return false if there is no next row 
 				return false;
 			}
 			
